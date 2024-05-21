f08638b24c3f5c55fe42299c79aaee5b30c4e69d
Galaxydragonx0 <andrewsieu@gmail.com>
2024-05-21 12:41:14 -0400

    trying somethingnew

--- back/svelte.config.js
+++ back/svelte.config.js
@@ -8,8 +8,19 @@ const config = {
     adapter: adapter(),
     paths: {
       base: process.argv.includes('dev') ?  '' : process.env.BASE_PATH,
-  }
+    },
+    prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}}
   },
-  preprocess: sequence([vitePreprocess(), preprocessMeltUI()])
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+  
 };
 export default config;
