490e893d5db44372195fb48439f3cca95e862fad
Galaxydragonx0 <andrewsieu@gmail.com>
2024-05-21 09:28:38 -0400

    jhgv

--- back/svelte.config.js
+++ back/svelte.config.js
@@ -1,28 +1,15 @@
 import {preprocessMeltUI, sequence} from "@melt-ui/pp";
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import adapter from '@sveltejs/adapter-static';
-// //const dev = "production" === "development";
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// kit: {
-// adapter: adapter({
-// pages: "docs",
-// assets: "docs"
-// }),
-// paths: {
-// // change below to your repo name
-// base: "" ,
-// },
-// // hydrate the <div id="svelte"> element in src/app.html
-// //target: "#svelte"
-// }
-// };
+
 import adapter from '@sveltejs/adapter-auto';
 /*@type {import('@sveltejs/kit').Config}*/
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/' : '',
+  }
   },
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()])
 };
