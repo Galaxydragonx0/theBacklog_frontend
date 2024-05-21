1a780148ce0927dcb848f87d17402eeecca3451b
Galaxydragonx0 <andrewsieu@gmail.com>
2024-05-21 11:14:49 -0400

    added in static site generation

--- back/svelte.config.js
+++ back/svelte.config.js
@@ -5,17 +5,9 @@ import adapter from '@sveltejs/adapter-static'
 
 const config = {
   kit: {
-    adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+    adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '' : '',
+      base: process.argv.includes('dev') ?  '' : process.env.BASE_PATH,
   }
   },
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()])
