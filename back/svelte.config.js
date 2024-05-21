0dea9a485c7afd7bc9eac74d6c9d986e5ce58613
Galaxydragonx0 <andrewsieu@gmail.com>
2024-05-21 10:48:22 -0400

    added in new adapter settings for deployment

--- back/svelte.config.js
+++ back/svelte.config.js
@@ -1,14 +1,21 @@
 import {preprocessMeltUI, sequence} from "@melt-ui/pp";
-import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static'
 
-import adapter from '@sveltejs/adapter-auto';
-/*@type {import('@sveltejs/kit').Config}*/
+
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/theBacklog_frontend' : '',
+      base: process.env.NODE_ENV === 'production' ? '' : '',
   }
   },
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()])
